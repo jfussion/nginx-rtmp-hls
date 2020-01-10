cc2e584bef79257f4c6dd3769227c2743f3e01fa
Joseph Bonilla <bonillajeb@gmail.com>
2020-01-10 17:18:10 +0800

    add zsh install script

--- istall.sh
+++ istall.sh
@@ -6,6 +6,10 @@ set -e
 apt update
 apt upgrade
 
+# install zsh
+apt install zsh
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
