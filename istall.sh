8f6c66c8d1ab462dfa58a10c037fc4dc6fca49b4
Joseph Bonilla <bonillajeb@gmail.com>
2020-01-10 17:44:51 +0800

    rename install script

---
+++ istall.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# update and upgrade
+apt update
+apt upgrade
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Install docker-compose
+apt install docker-compose
+
+# install zsh
+apt install zsh
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
